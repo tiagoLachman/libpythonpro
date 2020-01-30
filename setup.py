ab83d1347f5c30a728d39f98d73b157fd3fb5e82
Tchucknoia <tiagolachman@gmail.com>
2020-01-30 18:09:47 -0300

    Consertador bug na instalação via PyPi
    Esse é o release da versão 0.2

--- setup.py
+++ setup.py
@@ -95,7 +95,7 @@ def find_package_data(
 
 
 PACKAGE = "libpythonpro"
-NAME = PACKAGE
+NAME = "teste_do_noia"
 DESCRIPTION = "Módulo para exemplificar construção de projetos Python no curso PyTools"
 AUTHOR = "Tiago André Lachman"
 AUTHOR_EMAIL = "tiagolachman@gmail.com"
@@ -110,7 +110,7 @@ setup(
     long_description_content_type='text/markdown',
     author=AUTHOR,
     author_email=AUTHOR_EMAIL,
-    license=read('LICENSE'),
+    license="GNU AFFERO GENERAL PUBLIC LICENSE",
     url=URL,
     packages=find_packages(exclude=["tests.*", "tests"]),
     package_data=find_package_data(PACKAGE, only_in_packages=False),
